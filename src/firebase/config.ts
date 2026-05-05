7f3381a3b119d685a3cc3a3fb77a65558293468e
GoGroup <kantapat0263@gmail.com>
2026-05-05 03:34:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2099625459-19c42",
+  "appId": "1:889553695843:web:5b3e3a07855b6bc6417456",
+  "apiKey": "AIzaSyAsg5aLRYslfUGCO1TpsButbkB9ipq99Vs",
+  "authDomain": "studio-2099625459-19c42.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "889553695843"
+};
